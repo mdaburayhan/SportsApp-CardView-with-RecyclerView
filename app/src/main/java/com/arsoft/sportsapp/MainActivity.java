842f345c077c58343ac062e8b5432d4f64becf52
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-20 18:43:16 +0600

    Combined adapter,model class, cardView, RecycelerView using the MainActivity.java

--- app/src/main/java/com/arsoft/sportsapp/MainActivity.java
+++ app/src/main/java/com/arsoft/sportsapp/MainActivity.java
@@ -7,18 +7,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlin.jvm.internal.SpreadBuilder;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1- Adapter View
+    private RecyclerView recyclerView;
+
+    // 2- Data source
+    private List<Sport> sportList;
+
+    // 3- Adapter
+    private CustomAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("VolleyBall", R.drawable.volley);
+        Sport s4 = new Sport("Ping Pong", R.drawable.ping);
+        Sport s5 = new Sport("Tennis", R.drawable.tennis);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+        adapter = new CustomAdapter(sportList);
+
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
     }
 }
